2295e64977103ad98c745da8d7ed6d63c00ab269
Aniket <aniketgupta6394@gmail.com>
2025-09-12 16:53:24 +0530

    Add create user API

---
+++ app/apis/controllers/customers.controllers.js
@@ -0,0 +1,5 @@
+
+
+exports.Create = async (req, res) => {
+
+}
--- app/apis/routes/customer.routes.js
+++ app/apis/routes/customer.routes.js
@@ -1,9 +1,13 @@
 module.exports = (app) => {
+    const customers = require("../controllers/customers.controllers")
     const Auth = require("../../mongodb/schema/Auth.schema");
 
-    app.get("/customers", async (req, res) => { 
-              const data = await Auth.create({
-        text: "done",
-      });
-        res.json({ message: "List of customers" }); });
+    app.get("/create/user", customers.Create);
+
+    app.get("/customers", async (req, res) => {
+        const data = await Auth.create({
+            text: "done",
+        });
+        res.json({ message: "List of customers" });
+    });
 };
